84f8f37a8ce73e39520850e699bf78625f3105b4
tekna28 <salma.jeghloul@gmail.com>
2023-09-18 17:42:34 -0700

    update for main.h

--- main.h
+++ main.h
@@ -4,8 +4,23 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<stdarg.h>
+#include <limits.h>
 
-int_putchar(char c);
-int_printf(const char *format, ...);
+int _putchar(char c);
+int _printf(const char *format, ...);
+int printf_char(va_list value);
+int printf_percent(void);
+
+/**
+ * struct format - for the conversion specifiers of printf.
+ *
+ *@id: pointer to a char.
+ *@f: function pointer.
+ */
+typedef struct format
+{
+  char *id;
+  int (*f)();
+} format;
 
 #endif
